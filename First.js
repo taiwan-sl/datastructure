8b1149a7401cfcabb2cadfca499e72c72b0ffc4a
taiwan-sl <kfhwang@gm.nutc.edu.tw>
2026-09-08 16:07:57 +0800

    svg

--- First.js
+++ First.js
@@ -1,4 +1,4 @@
-var weight=60;
-var height= 160;
-var bmi=weight/(height/100**2);
-console.log("Your BMI:"+bmi);
+var _a_weight = 60;
+var _a_height = 160;
+var _a_bmi = weight / (height / 100 ** 2);
+console.log("Your BMI:" + bmi);
